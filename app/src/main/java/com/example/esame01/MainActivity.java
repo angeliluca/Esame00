f77ce98cbc4e33181e35b8d49ec765a322e95b9e
aletarozzi <alessia.tarozzi@gmail.com>
2019-11-26 14:17:30 +0100

    ale

--- app/src/main/java/com/example/esame01/MainActivity.java
+++ app/src/main/java/com/example/esame01/MainActivity.java
@@ -2,17 +2,23 @@
  Area commenti, vi chiedo di scrivere qui cose eventualmente utili agli altri.
  prova
  */
-package com.example.esame01;
+package com.example.rosetopersolino;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class main extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
     }
-}
+
+    public void visita(View view) {
+        Intent intent = new Intent(this, Visita.class);
+        startActivity(intent);
+    } }
