24c5dd9bcfd20e76e9b6b02c9ab1d0e564c7dd08
Giulia <giuliagamberini.g@gmail.com>
2019-12-08 23:11:30 +0100

    Giulia

---
+++ app/src/main/java/com/example/esame01/centifoliat.java
@@ -0,0 +1,18 @@
+package com.example.esame01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class centifoliat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_centifoliat);
+    }
+
+}
+
