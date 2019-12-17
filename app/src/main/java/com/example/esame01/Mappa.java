16dadcf1bdd39ab477eeebf041a8bc130f9788e2
Giulia <giuliagamberini.g@gmail.com>
2019-12-17 12:50:18 +0100

    Giulia

--- app/src/main/java/com/example/esame01/Mappa.java
+++ app/src/main/java/com/example/esame01/Mappa.java
@@ -3,6 +3,7 @@ package com.example.esame01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 
