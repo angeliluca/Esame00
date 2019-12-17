2aa1e6c29a0d18e5e0e74e003321d6974e812375
Giulia <giuliagamberini.g@gmail.com>
2019-12-17 12:01:26 +0100

    Giulia

--- app/src/main/java/com/example/esame01/Exit.java
+++ app/src/main/java/com/example/esame01/Exit.java
@@ -2,7 +2,12 @@ package com.example.esame01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+
+import static android.content.Intent.ACTION_VIEW;
 
 public class Exit extends AppCompatActivity {
 
@@ -11,4 +16,9 @@ public class Exit extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exit);
     }
+    public void open_Phone(View view) {
+        Intent intent = new Intent(ACTION_VIEW, Uri.parse("tel:0546 22932 "));
+        startActivity(intent);
+    }
+
 }
