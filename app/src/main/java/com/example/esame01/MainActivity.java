99c4e92ff62d4274959190fe45380a2a4a319216
Luca Angeli <lucaproange@gmail.com>
2019-12-18 15:36:41 +0100

    Luca

--- app/src/main/java/com/example/esame01/MainActivity.java
+++ app/src/main/java/com/example/esame01/MainActivity.java
@@ -8,10 +8,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 
 import com.example.esame01.R;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,7 +26,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    private void setAppLocale(String localeCode){
+        Resources resources = getResources();
+        DisplayMetrics dm = resources.getDisplayMetrics();
+        Configuration config = resources.getConfiguration();
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.JELLY_BEAN_MR1){
+            config.setLocale(new Locale(localeCode.toLowerCase()));
+        } else {
+            config.locale = new Locale(localeCode.toLowerCase());
+        }
+        resources.updateConfiguration(config, dm);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
     public void visita(View view) {
         Intent intent = new Intent(this, Visita.class);
         startActivity(intent);
-    } }
+    }
+
+    public void setEn(View view){
+        setAppLocale("en");
+    }
+
+    public void setFra(View view){
+        setAppLocale("fr");
+    }
+    public void setIt(View view){
+        setAppLocale("it");
+    }
+
+    public void setGer(View view){
+        setAppLocale("de");
+    }
+
+    public void setSpa(View view){
+        setAppLocale("es");
+    }
+
+
+
+}
